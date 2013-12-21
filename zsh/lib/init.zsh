0eebc0dc3a9728ed966d1a03ab30235afec193ba
Daniel Voogsgerd <daniel@voogsgerd.nl>
2013-12-21 02:38:03 +0100

    Removed warning in case nvm or nvm wasn't isntalled. Since I have no control over nvm or rvm on other servers.

--- zsh/lib/init.zsh
+++ zsh/lib/init.zsh
@@ -1,3 +1,7 @@
-source $HOME/.rvm/scripts/rvm
-source $HOME/.nvm/nvm.sh
+if [ -f $HOME/.rvm/scripts/rvm ];then
+	source $HOME/.rvm/scripts/rvm
+fi
+if [ -f $HOME/.nvm/nvm.sh ]; then
+	source $HOME/.nvm/nvm.sh
+fi
 . ~/Environment/zsh/z/z.sh
