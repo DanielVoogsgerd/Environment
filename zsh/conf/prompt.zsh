141bb10e5c7df71a90d81723b7aa70f6e5109438
Daniel Voogsgerd <daniel@voogsgerd.nl>
2014-02-18 10:42:22 +0100

    Changed prompt so I can see when I'm inside an ssh connection

--- zsh/conf/prompt.zsh
+++ zsh/conf/prompt.zsh
@@ -1,8 +1,22 @@
 source $ZSH/vcs/zshrc.sh
 
+SSH_CONNECTED_HOST=`who am i | awk -F"[()]" '{print $2}'`
+
 setopt PROMPT_SUBST
-PROMPT=$'%{${fg[green]}%}%n@%m:'  # Green color, current user@server
-PROMPT+='%{${fg[blue]}%}%~'       # Blue color, current directory, from $HOME
+if [[ "$USER" == "root" ]]; then
+	PROMPT=$'%{${fg[red]}%}%n'
+elif [[ "$USER" == "daniel" ]]; then
+	PROMPT=$'%{${fg[green]}%}%n'
+else
+	PROMPT=$'%{${fg[blue]}%}%n'
+fi
+PROMPT+='%{${fg[blue]}%}@'
+if [[ "$SSH_CONNECTED_HOST" == ":0" ]]; then
+	PROMPT+='%{${fg[blue]}%}%m'
+else
+	PROMPT+='%{${fg[red]}%}%m'
+fi
+PROMPT+='%{${fg[blue]}%}:%~'       # Blue color, current directory, from $HOME
 PROMPT+='%{${fg[default]}%}$(vcs_super_info)' # Default color, Git status
 PROMPT+='%{${fg[default]}%} $ ' # Default color, prompt
 
