6d319756871ccae1fc5930e65f0ada669c90f785
Daniel Voogsgerd <daniel@voogsgerd.nl>
2014-02-25 01:53:36 +0100

    Changed some things in prompt: Uppercase time etc

--- zsh/conf/prompt.zsh
+++ zsh/conf/prompt.zsh
@@ -1,14 +1,14 @@
 source $ZSH/vcs/zshrc.sh
 
 SSH_CONNECTED_HOST=`who am i | awk -F"[()]" '{print $2}'`
-
+PROMPT=$'%{${fg_bold[white]}%}'
 setopt PROMPT_SUBST
 if [[ "$USER" == "root" ]]; then
-	PROMPT=$'%{${fg[red]}%}%n'
+	PROMPT+='%{${fg[red]}%}%n'
 elif [[ "$USER" == "daniel" ]]; then
-	PROMPT=$'%{${fg[green]}%}%n'
+	PROMPT+='%{${fg[green]}%}%n'
 else
-	PROMPT=$'%{${fg[blue]}%}%n'
+	PROMPT+='%{${fg[blue]}%}%n'
 fi
 PROMPT+='%{${fg[blue]}%}@'
 if [[ "$SSH_CONNECTED_HOST" == ":0" || "$SSH_CONNECTED_HOST" == ":0.0" ]]; then
@@ -21,3 +21,4 @@ PROMPT+='%{${fg[default]}%}$(vcs_super_info)' # Default color, Git status
 PROMPT+='%{${fg[default]}%} $ ' # Default color, prompt
 
 
+RPROMPT=$'[%?][%*]'
