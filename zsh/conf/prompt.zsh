fe387fabd73c84a6ce462c182b14a3b1127c6586
Daniel Voogsgerd <daniel@voogsgerd.nl>
2014-02-26 18:49:32 +0100

    Undo

--- zsh/conf/prompt.zsh
+++ zsh/conf/prompt.zsh
@@ -20,4 +20,10 @@ PROMPT+='%{${fg[white]}%}:%{${fg[blue]}%}%~'       # Blue color, current directo
 PROMPT+='%{${fg[default]}%}$(vcs_super_info)' # Default color, Git status
 PROMPT+='%{${fg[default]}%} $ ' # Default color, prompt
 RPROMPT='%{${fg[default]}%}[%(?,%{${fg[green]}%}%?,%{${fg[red]}%}%?)%{${fg[default]}%}]'
+
+amihome > /dev/null
+if [[ "$?" == "Inside home" ]]; then
+	RPROMPT='test'
+fi
+
 RPROMPT+='%{${fg[default]}%}[%*]'
