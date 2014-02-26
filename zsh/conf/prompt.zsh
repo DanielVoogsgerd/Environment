92f3102cd629326e44620b983571ed6625449060
Daniel Voogsgerd <daniel@voogsgerd.nl>
2014-02-27 00:41:00 +0100

    Added a script that changes the title of the term.

--- zsh/conf/prompt.zsh
+++ zsh/conf/prompt.zsh
@@ -21,3 +21,33 @@ PROMPT+='%{${fg[default]}%}$(vcs_super_info)' # Default color, Git status
 PROMPT+='%{${fg[default]}%} $ ' # Default color, prompt
 RPROMPT='%{${fg[default]}%}[%(?,%{${fg[green]}%}%?,%{${fg[red]}%}%?)%{${fg[default]}%}]'
 RPROMPT+='%{${fg[default]}%}[%*]'
+
+# Besides an ordinairy prompt I also set the terminal window to contain the executed command. Thanks to Brian from bstpierre
+
+function title() {
+    # escape '%' chars in $1, make nonprintables visible
+    local a=${(V)1//\%/\%\%}
+
+    # Truncate command, and join lines.
+    a=$(print -Pn "%40>...>$a" | tr -d "\n")
+    case $TERM in
+        screen*)
+            print -Pn "\e]2;$a @ $2\a" # plain xterm title
+            print -Pn "\ek$a\e\\"      # screen title (in ^A")
+            print -Pn "\e_$2   \e\\"   # screen location
+            ;;
+        xterm*)
+            print -Pn "\e]2;$a @ $2\a" # plain xterm title
+            ;;
+    esac
+}
+
+# precmd is called just before the prompt is printed
+function precmd() {
+    title "zsh" "%m:%55<...<%~"
+}
+
+# preexec is called just before any command line is executed
+function preexec() {
+    title "$1" "%m:%35<...<%~"
+}
