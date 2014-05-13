af3c765d837ee472583c775e2840fe7855f2ec11
Daniel Voogsgerd <daniel@voogsgerd.nl>
2014-05-13 21:25:55 +0200

    The function never really made much sense

--- zsh/conf/function.zsh
+++ zsh/conf/function.zsh
@@ -28,22 +28,6 @@ function killuser {
 	skill -KILL -u "$1"
 }
 
-function fancyLocate {
-	locate "$1" | xargs ls -lsh
-}
-
-function compress {
-	if [ -f $1 ] || [ -d $1 ];then
-		if [ -f $2 ];then
-			print "The file already exists"
-		else
-			HandBrakeCLI -i $1 -o $2 -e x264 -q 20.0 -a 1,1 -E faac,copy:ac3 -B 160,160 -6 stereo -R Auto,Auto -D 0.0,0.0 -f mkv -4 --loose-anamorphic -m -x b-adapt=2:rc-lookahead=50
-		fi
-	else
-		print "Source doesn't exist"
-	fi
-}
-
 function sshmount {
 	if [[ -n "$1" && -n "$2" ]]; then
 		server=$(echo "$1"| tr '[:upper:]' '[:lower:]')
@@ -51,16 +35,6 @@ function sshmount {
 		sshfs "$1":"$2" ~/Servers/"$server" -C
 	else
 		echo "Unknown arguments.\nUsage: sshmount [FQDN] [REMOTE DIR]"
-	fi
-}
-
-function amihome {
-	if [[ "`pwd`" == $HOME || "`pwd`" == $HOME/* ]]; then
-		echo "Inside home"
-		return 0
-	else
-		echo "Outside home"
-		return 1
 	fi
 }
 
