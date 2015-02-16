342e7e43d9ec94a76b1a06bc9f6d79f55c931201
Daniel Voogsgerd <daniel@voogsgerd.nl>
2015-02-17 00:51:40 +0100

    Added changelog function

--- zsh/conf/debian.zsh
+++ zsh/conf/debian.zsh
@@ -1,7 +1,24 @@
 #!/usr/bin/zsh
 
 if [[ "$OS" == "Debian" ]]; then
-    alias apt-get="echo \"Stop doing that already, I've been replaced by apt.\""
-    alias update="apt update"
-    alias upgrade="apt upgrade"
+    if exist apt; then
+        alias apt-get="echo \"Stop doing that already, I've been replaced by apt.\""
+        alias apt-cache="echo \"Stop doing that already, I've been replaced by apt.\""
+    fi
+
+    function changelog() {
+        local location="/usr/share/doc/$1/changelog.Debian.gz"
+        if [ -f "$location" ]; then
+            zless "$location"
+        else
+            echo "No changelog found"
+        fi
+    }
+
+
+    alias pupdate="apt update"
+    alias pupgrade="apt upgrade"
+    alias psearch="apt search"
+
+    alias nuke="apt purge"
 fi
