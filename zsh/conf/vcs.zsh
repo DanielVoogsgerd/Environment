7a68be1ad52a86782f4ea5948817c50edbf925d9
Daniel Voogsgerd <daniel@voogsgerd.nl>
2015-10-14 23:59:30 +0200

    Style the prompt some more

---
+++ zsh/conf/vcs.zsh
@@ -0,0 +1,40 @@
+## Git without Action.
+# VCS name
+#ZSH_VCS_PROMPT_GIT_FORMATS='(%{%B%F{yellow}%}#s%{%f%b%})'
+# Branch name
+ZSH_VCS_PROMPT_GIT_FORMATS=' %{%B%F{red}%}#b%{%f%b%}'
+# Ahead and Behind
+ZSH_VCS_PROMPT_GIT_FORMATS+='#c#d|'
+# Staged
+ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{blue}%}#e%{%f%b%}'
+# Conflicts
+ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{red}%}#f%{%f%b%}'
+# Unstaged
+ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{yellow}%}#g%{%f%b%}'
+# Untracked
+ZSH_VCS_PROMPT_GIT_FORMATS+='#h'
+# Stashed
+ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{cyan}%}#i%{%f%b%}'
+# Clean
+ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{green}%}#j%{%f%b%}'
+
+
+
+# Branch name
+ZSH_VCS_PROMPT_GIT_ACTION_FORMATS='[%{%B%F{red}%}#b%{%f%b%}'
+# Action
+ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+=':%{%B%F{red}%}#a%{%f%b%}'
+# Ahead and Behind
+ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='#c#d|'
+# Staged
+ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{blue}%}#e%{%f%}'
+# Conflicts
+ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{red}%}#f%{%f%}'
+# Unstaged
+ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{yellow}%}#g%{%f%}'
+# Untracked
+ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='#h'
+# Stashed
+ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{cyan}%}#i%{%f%}'
+# Clean
+ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{green}%}#j%{%f%}]'
