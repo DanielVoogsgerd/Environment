493f7b117d796e5ac6de1b3f47342ae122e7dbf4
Daniel Voogsgerd <daniel@voogsgerd.nl>
2016-10-17 22:11:58 +0200

    Cleaned up prompt

--- zsh/conf/prompt.zsh
+++ zsh/conf/prompt.zsh
@@ -1,34 +1,12 @@
+setopt PROMPT_SUBST
 source $ZSH/vcs/zshrc.sh
 
 for color in {000..255}; do
     FG[$color]="%{[38;5;${color}m%}"
     BG[$color]="%{[48;5;${color}m%}"
 done
- 
-SSH_CONNECTED_HOST=`who am i | awk -F"[()]" '{print $2}'`
-PROMPT=$''
-setopt PROMPT_SUBST
-if [[ "$USER" == "root" ]]; then
-    PROMPT+='%{%F{001}%}%n'
-elif [[ "$USER" == "$username" ]]; then
-    PROMPT+='%{%F{002}%}%n'
-else
-    PROMPT+='%{%F{011}%}%n'
-fi
-PROMPT+='%{%F{004}%}@'
-if [[ "$SSH_CONNECTED_HOST" == ":0" || "$SSH_CONNECTED_HOST" == ":0.0" ]]; then
-    PROMPT+='%{%F{005}%}%m%f'
-else
-    PROMPT+='%{%F{009}%}%m'
-fi
-PROMPT+='%{%F{004}%}:%{%F{004}%}%~'       # Blue color, current directory, from $HOME
-PROMPT+='%{$reset_color%}$(vcs_super_info)' # Default color, Git status
-PROMPT+='%{%F{015}%} $ ' # Default color, prompt
-RPROMPT='%{%F{004}%}[%(?,%{%F{012}%}%?,%{%F{001}%}%?)%{%F{004}%}]'
-RPROMPT+='%F{004}[%F{012}%*%F{004}]%f'
 
 # Besides an ordinairy prompt I also set the terminal window to contain the executed command. Thanks to Brian from bstpierre
-
 function title() {
     # escape '%' chars in $1, make nonprintables visible
     local a=${(V)1//\%/\%\%}
@@ -47,6 +25,16 @@ function title() {
     esac
 }
 
+# precmd is called just before the prompt is printed
+function precmd() {
+    title "zsh" "%m:%55<...<%~"
+}
+
+# preexec is called just before any command line is executed
+function preexec() {
+    title "$1" "%m:%35<...<%~"
+}
+
 function gen_clc() {
     echo "%{%F{$1}%}"
 }
@@ -57,18 +45,6 @@ clc_hostname="005"
 clc_path="027"
 clc_reset="$reset_color"
 clc_end="015"
-
-if [[ "$USER" == "$username" ]]; then
-    clc_name="002"
-elif [[ "$USER" == "root" ]]; then
-    clc_name="001"
-fi
-
-if [[ -z "$SSH_CONNECTED" ]]; then
-	clc_hostname="005"
-else
-	clc_hostname="009"
-fi
 
 function prompt() {
     local PCLC_NAME=$(gen_clc "$clc_name")
@@ -81,7 +57,7 @@ function prompt() {
 	PROMPT=""
 
 	if [[ "$USER" != "$username" ]]; then
-		PROMPT+="$PCLC_NAME%n"
+		PROMPT+="$PCLC_NAME%n "
 	fi
 
 	if [[ -n "$SSH_CONNECTION" && -z "$TMUX" ]]; then
@@ -91,13 +67,3 @@ function prompt() {
 	PROMPT+="$PCLC_PATH%~%f"'$(vcs_super_info_wrapper)'"$PCLC_END "'$(shell_icon)'" "
 }
 prompt
-
-# precmd is called just before the prompt is printed
-function precmd() {
-    title "zsh" "%m:%55<...<%~"
-}
-
-# preexec is called just before any command line is executed
-function preexec() {
-    title "$1" "%m:%35<...<%~"
-}
